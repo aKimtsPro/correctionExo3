f60c92e0b39ff0d2bfef3dd5e0800364fb779bf7
akimt <a.kimtsaris@outlook.com>
2020-11-27 17:09:16 +0100

    to mappers

--- src/main/java/bstorm/akim/correctionExo3/SecurityConfig.java
+++ src/main/java/bstorm/akim/correctionExo3/SecurityConfig.java
@@ -3,6 +3,7 @@ package bstorm.akim.correctionExo3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/section/**", "/section").authenticated()
+                .antMatchers("/student/**", "/student").hasRole("USER")
+                .antMatchers(HttpMethod.GET).permitAll()
+                .antMatchers(HttpMethod.DELETE).authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
